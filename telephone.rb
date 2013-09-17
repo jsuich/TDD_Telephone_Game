24f9a142a3d1ae55da71e139948f6d403e13653f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 17:40:57 -0500

    '~Done'

--- telephone.rb
+++ telephone.rb
@@ -1,26 +1,42 @@
-# class Pizza
-#   attr_accessor :toppings
 
-#   def initialize(cook_time, toppings_array = [])
-#     @toppings = toppings_array
-#   end
 
-#   def addTopping(topping)
-#     toppings << topping
-#   end
-# end
+class Pizza
+  attr_accessor :toppings
+  @max_topping_size = {small: 2, medium: 3, large: 4}
+
+  def initialize(size, cook_time)
+    @toppings = toppings_array
+  end
+
+  def addTopping(topping)
+    if @toppings.size  < max_topping_size
+      toppings << topping
+    end
+  end
+end
+
+
+
+
+
 
 class Topping
   attr_reader :cost, :name
+
   def initialize(topping_name, cost = 1)
     @name = name
     @cost = cost
+    @smells_ok = false
+  end
+
+  def smells_ok?
+    return smells_ok
   end
 end
 
 
 # class PizzaOrderingSystem
 
-# do		
+# end 
 
 
--- telephone_spec.rb
+++ telephone_spec.rb
@@ -1,15 +1,68 @@
 require 'rspec'
 
 require "spec_helper"
-require "telephone"
+require_relative "telephone"
 
 describe Topping do 
-  it "should require a topping name" do 
-    expect{Topping.new()}.to raise_error
+  context 'testing class' do 
+    it "should require a topping name" do 
+      expect{Topping.new()}.to raise_error
+      expect{Topping.new("poop")}.to_not raise_error
+      expect{Topping.new("poop", 2)}.to_not raise_error
+    end
+
+    it "has necessary methods" do 
+      should respond_to :cost
+      should respond_to :name
+      should respond_to :smells_ok?
+    end
   end
 
+  context 'instance of topping' do
+    let!(:my_topping){Topping.new("slow child", 2)}
+
+    it "has a boolean smell" do 
+      expect{my_topping.smells_ok? == true || my_topping.smells_ok? == false}.should be_true
+    end
+  end
+
+
+
+
+  # it { responds_to }
 end
 
-describe PizzaOrderingSystem do
+# describe PizzaOrderingSystem do
 
-end
+# end
+
+# describe PizzaOrderingSystem do 
+#   context 'testing class' do 
+#|
+#|
+#|
+#     it "should require a topping name" do 
+#       expect{Topping.new()}.to raise_error
+#       expect{Topping.new("poop")}.to_not raise_error
+#       expect{Topping.new("poop", 2)}.to_not raise_error
+#     end
+
+#     it "has necessary methods" do 
+#       should respond_to :cost
+#       should respond_to :name
+#       should respond_to :smells_ok?
+#     end
+#   end
+
+#   context 'instance of topping'
+#     let!(:my_topping){Topping.new("slow child", 2)}
+
+#     it "has a boolean smell" do 
+#       expect{my_topping.smells_ok? == true || my_topping.smells_ok? == false}.should be_true
+#     end
+#   end
+
+
+
+#   # it { responds_to }
+# end
