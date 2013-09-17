503a62b95ab03ed431b44759d21e28ede64d2184
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 16:52:08 -0500

    Just created topping. Now creating tests

--- telephone.rb
+++ telephone.rb
@@ -1,9 +1,26 @@
-class KillerZoo
+# class Pizza
+#   attr_accessor :toppings
 
+#   def initialize(cook_time, toppings_array = [])
+#     @toppings = toppings_array
+#   end
+
+#   def addTopping(topping)
+#     toppings << topping
+#   end
+# end
+
+class Topping
+  attr_reader :cost, :name
+  def initialize(topping_name, cost = 1)
+    @name = name
+    @cost = cost
+  end
 end
 
 
+# class PizzaOrderingSystem
 
-class PizzaOrderingSystem
+# do		
 
-do		
+
--- telephone_spec.rb
+++ telephone_spec.rb
@@ -1,8 +1,12 @@
 require 'rspec'
 
 require "spec_helper"
+require "telephone"
 
-describe KillerZoo do 
+describe Topping do 
+  it "should require a topping name" do 
+    expect{Topping.new()}.to raise_error
+  end
 
 end
 
